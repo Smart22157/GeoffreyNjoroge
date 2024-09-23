78abed39375faba412c06e7b8ed64b7208616065
smartjeff532 <smartjeff532@gmail.com>
2024-09-23 11:48:59 +0300

    update

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Geoffrey_Muchangi.Models;
+using System;
+using System.Windows.Forms;
+
+namespace Geoffrey_Muchangi.Models
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // Ensure this points to your form
+        }
+    }
+}
